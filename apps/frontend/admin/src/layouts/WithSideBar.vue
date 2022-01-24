f6d66021d3da084f9b3647796167998b1cc7d075
Aabid <mailtoaabid01@gmail.com>
2022-01-24 22:42:53 +0530

    added vuex vue-router

---
+++ apps/frontend/admin/src/layouts/WithSideBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">"sidebar"</div>
+        <div class="col-md-9">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
